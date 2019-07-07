5ea87f455c13eb1c7d788753cd1a5f7611bb6b25
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-07-07 18:08:35 +0530

    Add files via upload

---
+++ TensorFlow.py
@@ -0,0 +1,4 @@
+Tensorflow there sre the classifier and the regreesion also.
+sciket learn is the library which cannot think about like humans.
+                 we want a program which treats likw a human...and it know as brain.
+   
