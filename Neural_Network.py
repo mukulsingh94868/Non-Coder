7576a2f5b0e1b7f295f5f929dd9cf877bc29d702
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-07-07 18:08:55 +0530

    Add files via upload

---
+++ Neural_Network.py
@@ -0,0 +1,45 @@
+Neural Network
+
+
+
+--> there are differnrt kinds of neural networks.
+   where there is called the Artificial Neural Networks (ANN)
+   ...  there are the inputs called the dendrites..
+and there aur transport of outpts and storing of data called the AXON.
+
+
+---->  input meanas no. of attributes/data... 
+       when we make a perfect ANN where we have one output and one input layer.
+        input and hidden layers are called the layers which is decided by us.
+       neural is the gpu of any systems...
+        the first hidden layer process the data firstly...
+       there is activation function where we have to process the data by the hidden layer...
+       
+
+
+
+.....>  the first layer is the input layer where we give the inputs..
+        the second layer is the hidden layer and its all about our choice where we can decide how much we can contribute
+            every neuron of input layer is connected with each neuron of the hidden layer...
+        if we use the hidden layer that much we also need the tpu,gpu and cpu for the better processing...
+        the mathematical equation use in this for processing that function is known as Activation Function...
+
+
+......> (Activation Function) ...if there is more than two hidden layers so it is called the deep Neural Networks...
+                   and output is the processung part between the hidden layers... and it is called the output...
+                   there is cost function where is used for find out the errors...
+                     
+                 use cases...
+                 ..> performance search
+                 ...>  voice procesing
+                 ...>  self driver car
+                   ...>  computer games
+ .......>    no.of attribute is described into the input layer...it means each and every attribute should describe the neuron..
+
+                          
+        
+               y=m1x1+m2x2+m3x3+b
+                   where 'm' is the  weight.
+    ......> there is a processs which called the Back Propogation... in which it changes into the weights in the input layer.
+
+
